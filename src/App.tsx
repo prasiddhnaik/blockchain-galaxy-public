6358228790fd3ab23ae40d68924a7eb744033dee
Prasiddh Naik <legoprasiddh@gmail.com>
2026-06-28 14:20:40 +0530

    Revert "Add hackathon thumbnail scene"
    This reverts commit cbc323a67b49f73375fbe35f761741ab2e37060d.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
 import './App.css'
 import { Galaxy } from './scene/Galaxy'
-import { ThumbnailScene } from './scene/ThumbnailScene'
 
 function App() {
-  const searchParams = new URLSearchParams(window.location.search)
-  const isThumbnailMode =
-    searchParams.has('thumbnail') || window.location.hash === '#thumbnail'
-
-  if (isThumbnailMode) {
-    return <ThumbnailScene />
-  }
-
   return <Galaxy />
 }
 
